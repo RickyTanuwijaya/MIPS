48cbd099912471603c7d19600bb49b941265f6ff
Ricky Tanuwijaya <84340225+RickyTanuwijaya@users.noreply.github.com>
2025-06-16 07:35:37 +0700

    Update alu.v

--- alu.v
+++ alu.v
@@ -1,30 +1,71 @@
-`include "fullAdder.v"
-`include "fullAddSub32.v"
+`include "./fullAdder.v"
+`include "./fullAddSub32.v"
 
 module ALUControl (
-    input [1:0] ALUOp,
-    input [5:0] funct,
-    output reg [3:0] aluCtrl
+    input  wire [1:0] ALUOp,    // dari control unit
+    input  wire [5:0] funct,    // dari field funct di instruksi R-type
+    output reg  [3:0] aluCtrl   // untuk ke ALU utama
 );
 
-    always @(*) begin
-        case (ALUOp)
-            2'b00: aluCtrl = 4'b0010; // LW or SW → ADD
-            2'b01: aluCtrl = 4'b0110; // BEQ → SUB
-            2'b10: begin // R-type
-                case (funct)
-                    6'b100000: aluCtrl = 4'b0010; // ADD
-                    6'b100010: aluCtrl = 4'b0110; // SUB
-                    6'b100100: aluCtrl = 4'b0000; // AND
-                    6'b100101: aluCtrl = 4'b0001; // OR
-                    6'b101010: aluCtrl = 4'b0111; // SLT
-                    default:   aluCtrl = 4'b1111; // INVALID
-                endcase
-            end
-            default: aluCtrl = 4'b1111; // INVALID
+parameter ADDFN  = 6'b100000;
+parameter ADDUFN = 6'b100001;
+parameter SUBFN  = 6'b100010;
+parameter SUBUFN = 6'b100011;
+parameter ANDFN  = 6'b100100;
+parameter ORFN   = 6'b100101;
+parameter SLLFN  = 6'b000000;
+parameter SRLFN  = 6'b000010;
+parameter SLTFN  = 6'b101010;
+
+reg [3:0] fno;
+/* aluCtrl: 
+    0: Add
+    1: addu
+    2: subtract
+    3: subu
+    4: and
+    5: or
+    6: sll
+    7: srl
+    8: slt
+//    9: lw , sw
+10  9: beq
+11  10: bne
+
+*/
+
+    always @ (funct)
+    begin
+        case(funct)
+            ADDFN: fno = 4'b0000;
+            ADDUFN: fno = 4'b0001;
+            SUBFN: fno = 4'b0010;
+            SUBUFN: fno = 4'b0011;
+            ANDFN: fno = 4'b0100;
+            ORFN: fno = 4'b0101;
+            SLLFN: fno = 4'b0110;
+            SRLFN: fno = 4'b0111;
+            SLTFN: fno = 4'b1000;
+            default: fno = 4'b0000;
         endcase
     end
 
+/* ALUOP: 2 bits: 
+00 RTYPE
+01 SW LW ADDI 
+10 BEQ
+11 BNE
+*/
+    always @ (ALUOp)
+    begin
+        case(ALUOp)
+            2'b00: aluCtrl = fno;
+            2'b01: aluCtrl = 4'b0000;// normal ADD op but immediate source 
+            2'b10: aluCtrl = 4'b1001;//9
+            2'b11: aluCtrl = 4'b1010;//10
+            default: aluCtrl = 4'b0000;
+        endcase
+    end
 endmodule
 
 
@@ -87,3 +128,4 @@ module alu(
     end
 
 endmodule
+
